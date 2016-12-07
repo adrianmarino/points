f5aefe29c80babb493ef0929a3b7743c190995dc
adrianmarino <adrianmarino@gmail.com>
2016-12-06 22:58:03 -0300

    improve test comprehension

--- spec/point/services/deposit_service_spec.exs
+++ spec/point/services/deposit_service_spec.exs
@@ -1,33 +1,31 @@
 defmodule Point.DepositServiceSpec do
   use ESpec
+  import ServiceSpecHelper
   alias Point.AccountFactory
   import ServiceSpecHelper
   import Point.Repo
   import Decimal
-  import ServiceSpecHelper
 
-  let amount: Decimal.new 100.1234567891
+  let amount: Decimal.new 10.12
+  let deposit: ok_result(described_module.deposit(amount: amount, on: account))
 
-  describe "deposit" do
-    let deposit: ok_result(described_module.deposit(amount: amount, on: account))
+  context "when deposit an amount to issuer backup account" do
+    let account: AccountFactory.insert(:revel_backup)
 
-    context "when deposit an amount to issuer backup account" do
-      let account: AccountFactory.insert(:revel_backup)
-
-      it "should increases account balance for deposited amount" do
-        expect fn-> deposit end |> to(change amount(account), round(add(account.amount, amount),2))
-      end
-
-      it "creates a movement with account as target acount" do
-        expect(assoc(deposit, :target).id).to(eq account.id)
-      end
-
-      it "creates a movement with deposited amount", do: expect(deposit.amount).to(eq amount)
+    it "should increases account balance up to deposited amount" do
+      expect fn-> deposit end |> to(change amount(account), plus(account, amount))
     end
 
-    context "when deposit an amount to user account" do
-      let account: AccountFactory.insert(:obiwan_kenoby_revel)
-      it "should raise and error", do: expect fn-> deposit end |> to(raise_exception)
+    it "should creates a movement with the account as target acount" do
+      expect(assoc(deposit, :target).id).to(eq account.id)
     end
+
+    it "should creates a movement with the deposited amount", do: expect(deposit.amount).to(eq amount)
+  end
+
+  context "when deposit an amount to default account" do
+    let account: AccountFactory.insert(:obiwan_kenoby_revel)
+
+    it "should raises and error", do: expect fn-> deposit end |> to(raise_exception)
   end
 end
--- spec/point/services/extract_service_spec.exs
+++ spec/point/services/extract_service_spec.exs
@@ -5,26 +5,24 @@ defmodule Point.ExtractServiceSpec do
   import Decimal
   alias Point.{ExchangeRate, AccountFactory}
 
-  describe "extract" do
-    let extract: ok_result(described_module.extract(amount: amount, from: backup))
-    let backup: AccountFactory.insert(:revel_backup)
-    let account: AccountFactory.insert(:han_solo_revel, issuer: backup.owner)
-    let issuer_owner_rate: %ExchangeRate{value: 5, source: backup.currency, target: account.currency}
+  let backup: AccountFactory.insert(:revel_backup)
+  let account: AccountFactory.insert(:han_solo_revel, issuer: backup.owner)
+  let issuer_owner_rate: %ExchangeRate{value: 5, source: backup.currency, target: account.currency}
+  let extract: ok_result(described_module.extract(amount: amount, from: backup))
 
-    before do: insert!(issuer_owner_rate)
+  before do: insert!(issuer_owner_rate)
 
-    context "when extract an amount from backup account that has more than needed backup" do
-      let amount: Decimal.new 10.12
+  context "when extract an amount from backup account that has more than needed backup" do
+    let amount: Decimal.new 10.12
 
-      it "should decrements backup account to required amount" do
-        expect fn-> extract end |> to(change amount(backup), round(sub(backup.amount, amount), 2))
-      end
+    it "should decrements backup account to required amount" do
+      expect fn-> extract end |> to(change amount(backup), minus(backup, amount))
     end
+  end
 
-    context "when extract an amount from backup account that has less than needed backup" do
-      let amount: Decimal.new 15000
+  context "when extract an amount from backup account that has less than needed backup" do
+    let amount: Decimal.new 15000
 
-      it "shouldn't allows extract required amount", do: expect fn-> extract end |> to(raise_exception)
-    end
+    it "shouldn't allows extract required amount", do: expect fn-> extract end |> to(raise_exception)
   end
 end
