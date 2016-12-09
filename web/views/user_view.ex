7e3470460168a1e36812648a3beb9e4e212d7a97
adrianmarino <adrianmarino@gmail.com>
2016-12-09 19:10:53 -0300

    order fields

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -7,6 +7,6 @@ defmodule Point.UserView do
   def render("show.json", %{user: user}), do: render_one(user, UserView, "user.json")
 
   def render("user.json", %{user: user}) do
-    %{id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email }
+    %{email: user.email, first_name: user.first_name, last_name: user.last_name}
   end
 end
