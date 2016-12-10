0dc8bd002dfb5feff8d1263e62d9f8a3599e1084
adrianmarino <adrianmarino@gmail.com>
2016-12-10 14:51:46 -0300

    doesnt remove fields

--- lib/point/services/session_service.ex
+++ lib/point/services/session_service.ex
@@ -2,19 +2,15 @@ defmodule Point.SessionService do
   import Ecto.Query
   import Point.Ecto.DateTimeUtil
   import Enum
-  import Map, only: [put: 3, delete: 2]
+  import Map, only: [put: 3]
   alias Point.{Repo, Session, User}
 
   def all() do
     Repo.all(
       from s in Session, join: u in User,
       where: s.user_id == u.id,
-      select: %{created_at: s.inserted_at, ttl: s.ttl, email: u.email, token: s.token }
-    ) |> map fn session->
-        put(session, :seconds_left, sec_left(session.created_at, session.ttl))
-          |> delete(:created_at)
-          |> delete(:ttl)
-    end
+      select: %{inserted_at: s.inserted_at, ttl: s.ttl, email: u.email, token: s.token }
+    ) |> map (&(put(&1, :seconds_left, sec_left(&1.inserted_at, &1.ttl))))
   end
 
   def open(for_user: user) do
