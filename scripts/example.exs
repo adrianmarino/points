08182a68d0a5aedcfa214769e95c0539c4dfd61e
adrianmarino <adrianmarino@gmail.com>
2017-01-02 08:48:28 -0300

    extract execution invocation

--- scripts/example.exs
+++ scripts/example.exs
@@ -15,13 +15,3 @@ defmodule Example do
   defp to(params), do: account(email: params.to.email, currency: params.to.currency)
   defp from(params), do: account(email: params.from.email, currency: params.from.currency)
 end
-
-require Engine
-
-Engine.run(
-  Example,
-  params: %{
-    from: %{email: "obiwankenoby@gmail.com",    currency: "RBL"},
-    to:   %{email: "anakinskywalker@gmail.com", currency: "EMP"}
-  }
-)
---
+++ scripts/runner.exs
@@ -0,0 +1,10 @@
+require Engine
+Code.require_file("scripts/example.exs")
+
+Engine.run(
+  Example,
+  params: %{
+    from: %{email: "obiwankenoby@gmail.com",    currency: "RBL"},
+    to:   %{email: "anakinskywalker@gmail.com", currency: "EMP"}
+  }
+)
