37b378f3e4e19bf3d62d64efbd38325792e8ffe1
adrianmarino <adrianmarino@gmail.com>
2017-01-08 16:48:21 -0300

    uncomment String.Charts implementation for {:ok, %Movement{}}

--- lib/point/extensions/to_string.ex
+++ lib/point/extensions/to_string.ex
@@ -1,12 +1,12 @@
 import Kernel, except: [to_string: 1]
-# defimpl String.Chars, for: Tuple do
-#   def to_string(value) do
-#     case value do
-#       {:ok, value} -> Point.Model.to_string(value)
-#       value -> to_string value
-#     end
-#   end
-# end
+defimpl String.Chars, for: Tuple do
+  def to_string(value) do
+    case value do
+      {:ok, value} -> Point.Model.to_string(value)
+      value -> to_string value
+    end
+  end
+end
 defimpl String.Chars, for: Map do
   def to_string(value), do: inspect(value)
 end
