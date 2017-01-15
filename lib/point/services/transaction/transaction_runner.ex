07d911ee75694833a892d9da3f097d91234b1d7e
adrianmarino <adrianmarino@gmail.com>
2017-01-14 21:13:49 -0300

    improve modules responsabilities and log messages

--- lib/point/services/transaction/transaction_runner.ex
+++ lib/point/services/transaction/transaction_runner.ex
@@ -1,39 +1,44 @@
 defmodule Point.TransactionRunner do
   import Macro, only: [camelize: 1]
-  require Logger
-  alias Point.{Config, MapUtil, FileUtil}
+  import Point.FileUtil, only: [last_write: 1]
+  import Point.MapUtil, only: [keys_to_atom: 1]
+  import Logger
+  alias Point.Config
 
   def execute(transaction, params) do
     try do
-      require_script(transaction)
-      exec_script(transaction, params)
+      require_source(transaction)
+      exec_source(transaction, params)
     rescue
-      e -> {:error, e}
+      error -> {:error, error}
     end
   end
 
-  defp exec_script(transaction, params) do
-    exec_src = "Engine.run(#{camelize transaction.name}, params)"
-    Logger.info "Execute: #{exec_src}"
-    {result, _} = Code.eval_string(exec_src, [params: MapUtil.keys_to_atom(params)], __ENV__)
+  defp exec_source(transaction, params) do
+    {result, _} = Code.eval_string(
+      "Engine.run(#{camelize transaction.name}, params)",
+      [params: keys_to_atom(params)],
+      __ENV__
+    )
+    info "Engine.run(#{camelize transaction.name}, #{keys_to_atom(params)}) was executed!"
     result
   end
 
-  defp require_script(transaction) do
-    path = script_path(transaction.name)
+  defp require_source(transaction) do
+    File.mkdir(tmp_path)
+    path = "#{tmp_path}/#{transaction.name}.exs"
 
-    FileUtil.was_writen(path, before_that: transaction.updated_at, then: fn ->
-      File.write!(path, transaction.source)
-      Logger.info "#{path} script was updated!"
-    end)
-
-    Logger.info "Require: #{path}"
-    Code.require_file(path)
+    case last_write(path) do
+      {:ok, last_write} ->
+        if Timex.compare(transaction.updated_at, last_write) > 0,
+          do: write_and_require(path, transaction.source); info "#{path} script was updated and required!"
+      _ ->  write_and_require(path, transaction.source); info "#{path} script was created and required!"
+    end
   end
 
-  defp script_path(filename) do
-    File.mkdir tmp_path
-    "#{tmp_path}/#{filename}.exs"
+  defp write_and_require(path, content) do
+    File.write!(path, content)
+    Code.require_file(path)
   end
 
   defp tmp_path, do: Config.get(:tmp_path)
--- lib/point/util/file_util.ex
+++ lib/point/util/file_util.ex
@@ -1,19 +1,8 @@
 defmodule Point.FileUtil do
-  def write_datetime(path) do
+  def last_write(path) do
     case File.stat(path) do
       {:ok, stat} -> Timex.to_datetime(stat.atime)
       error -> error
-    end
-  end
-
-  def was_writen(path, before_that: datetime, then: block) do
-    case write_datetime(path) do
-      {:ok, write_datetime} ->
-        case Timex.compare(datetime, write_datetime) do
-          result when result < 0 -> block.()
-          _ -> true
-        end
-      _ -> block.()
     end
   end
 end
