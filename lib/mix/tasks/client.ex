ee30ea7966f9fb386450ec7b48dd49e9b62e11d0
adrianmarino <adrianmarino@gmail.com>
2017-01-24 14:25:26 -0300

    nest client tasks by resource name

--- lib/mix/tasks/client.ex
+++ lib/mix/tasks/client.ex
@@ -1,17 +1,17 @@
-defmodule Mix.Tasks.Point do
-  defmodule SignIn do
+defmodule Mix.Tasks.Point.Client do
+  defmodule Sessions.SignIn do
     use Mix.Task.PointClient
     @shortdoc "Open a session"
     defrun fn(params)-> points(base_url) |> sign_in(email: first(params), password: last(params)) end
   end
-  defmodule SignOut do
+  defmodule Sessions.SignOut do
     use Mix.Task.PointClient
     @shortdoc "Close a session"
     defrun fn(params)-> points(base_url, first(params)) |> sign_out end
   end
-  defmodule Sessions do
+  defmodule Sessions.All do
     use Mix.Task.PointClient
-    @shortdoc "Show opened sessions"
+    @shortdoc "Show all sessions"
     defrun fn(params)-> points(base_url, first(params)) |> sessions end
   end
 end
