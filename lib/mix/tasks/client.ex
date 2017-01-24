b3eb2e3fdc673c82188539239098b8b246dea910
adrianmarino <adrianmarino@gmail.com>
2017-01-24 17:22:01 -0300

    rename task sessions.all to sessions

--- lib/mix/tasks/client.ex
+++ lib/mix/tasks/client.ex
@@ -2,38 +2,36 @@ defmodule Mix.Tasks.Point.Client do
   defmodule Sessions.SignIn do
     use Mix.Task.PointClient
     @shortdoc "Open a session"
-    defrun fn(params)-> points(base_url) |> sign_in(email: first(params), password: last(params)) end
+    defrun fn([email | password])-> points(base_url) |> sign_in(email: email, password: password) end
   end
   defmodule Sessions.SignOut do
     use Mix.Task.PointClient
     @shortdoc "Close a session"
-    defrun fn(params)-> points(base_url, first(params)) |> sign_out end
+    defrun fn([token | _])-> points(base_url, token) |> sign_out end
   end
-  defmodule Sessions.All do
+  defmodule Sessions do
     use Mix.Task.PointClient
     @shortdoc "Show all sessions"
-    defrun fn(params)-> points(base_url, first(params)) |> sessions end
+    defrun fn([token | _])-> points(base_url, token) |> sessions end
   end
   defmodule Users do
     use Mix.Task.PointClient
     @shortdoc "Show users"
-    defrun fn(token)-> points(base_url, token) |> users end
+    defrun fn([token | _])-> points(base_url, token) |> users end
   end
   defmodule Users.Add do
     use Mix.Task.PointClient
     @shortdoc "Add a user. Params: token email password first_name last_name"
-    alias Point.Client.User
-    defrun fn([token | user])-> points(base_url, token) |> add_user(User.create(user)) end
+    defrun fn([token | user])-> points(base_url, token) |> add_user(Point.Client.User.create(user)) end
   end
   defmodule Users.Update do
     use Mix.Task.PointClient
     @shortdoc "Update a user. Params: token email password first_name last_name"
-    alias Point.Client.User
-    defrun fn([token | user])-> points(base_url, token) |> update_user(User.create(user)) end
+    defrun fn([token | user])-> points(base_url, token) |> update_user(Point.Client.User.create(user)) end
   end
   defmodule Users.Delete do
     use Mix.Task.PointClient
     @shortdoc "Delete a user. Params: token email"
-    defrun fn([token |[email | _]])-> points(base_url, token) |> delete_user(email: email) end
+    defrun fn([token | [email | _]])-> points(base_url, token) |> delete_user(email: email) end
   end
 end
