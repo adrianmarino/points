2df0a92113c6fd955cd027d94508ecc134aab473
adrianmarino <adrianmarino@gmail.com>
2017-02-20 23:48:51 -0300

    reuse ecto query

--- lib/point/services/account_service.ex
+++ lib/point/services/account_service.ex
@@ -29,31 +29,19 @@ defmodule Point.AccountService do
   def get(id), do: Repo.get(Account, id)
 
   def by(email: email, currency_code: currency_code) do
-    Repo.one(from a in Account, join: c in Currency, join: u in User,
-             where: a.currency_id == c.id and a.owner_id == u.id and c.code == ^currency_code and u.email == ^email)
+    Repo.one(
+      from a in Account,
+      join: c in Currency, join: u in User,
+      where: a.currency_id == c.id and a.owner_id == u.id and c.code == ^currency_code and u.email == ^email)
   end
 
   def by(currency_code: currency_code) do
     Repo.all(from a in Account, join: c in Currency, where: a.currency_id == c.id and c.code == ^currency_code)
   end
 
-  def by(entity: entity) do
-    Repo.all(
-      from a in Account,
-      join: u in User,
-      join: ue in "users_entities",
-      where: a.issuer_id == u.id and u.id == ue.user_id and ue.entity_id == ^entity.id
-    )
-  end
-  def count_by(entity: entity) do
-    Repo.one(
-      from a in Account,
-      join: u in User,
-      join: ue in "users_entities",
-      where: a.issuer_id == u.id and u.id == ue.user_id and ue.entity_id == ^entity.id,
-      select: count(a.id)
-    )
-  end
+  def by(entity: entity), do: Repo.all(by_entity_query entity)
+
+  def count_by(entity: entity), do: Repo.one(from a in by_entity_query(entity), select: count(a.id))
 
   def backup_account_of(account) do
     Repo.one(from a in Account, where: a.type == "backup" and a.owner_id == ^account.issuer_id)
@@ -62,6 +50,13 @@ defmodule Point.AccountService do
   def increase_changeset(account, amount), do: Account.changeset(account, %{amount: add(account.amount, new(amount))})
 
   def decrease_changeset(account, amount), do: Account.changeset(account, %{amount: sub(account.amount, new(amount))})
+
+  defp by_entity_query(entity) do
+    from a in Account,
+    join: u in User,
+    join: ue in "users_entities",
+    where: a.issuer_id == u.id and u.id == ue.user_id and ue.entity_id == ^entity.id
+  end
 
   defp insert_changeset(params), do: Account.insert_changeset(%Account{}, Map.merge(%{"type" => "default"}, params))
 end
--- lib/point/services/user/user_service.ex
+++ lib/point/services/user/user_service.ex
@@ -13,22 +13,9 @@ defmodule Point.UserService do
 
   def by(email: email), do: Repo.get_by(User, email: email)
 
-  def by(entity: entity) do
-    Repo.all(
-      from u in User,
-      join: ue in "users_entities",
-      where: u.id == ue.user_id and ue.entity_id == ^entity.id
-    )
-  end
+  def by(entity: entity), do: Repo.all(by_entity_query(entity))
 
-  def count_by(entity: entity) do
-    Repo.one(
-      from u in User,
-      join: ue in "users_entities",
-      where: u.id == ue.user_id and ue.entity_id == ^entity.id,
-      select: count(u.id)
-    )
-  end
+  def count_by(entity: entity), do: Repo.one(from u in by_entity_query(entity), select: count(u.id))
 
   def register(params), do: Repo.insert(User.insert_changeset(%User{}, params))
 
@@ -37,4 +24,10 @@ defmodule Point.UserService do
   def update(email, params), do: Repo.update(User.update_changeset(by(email: email), params))
 
   def delete!(email: email), do: Repo.delete!(by(email: email))
+
+  defp by_entity_query(entity) do
+    from u in User,
+    join: ue in "users_entities",
+    where: u.id == ue.user_id and ue.entity_id == ^entity.id
+  end
 end
