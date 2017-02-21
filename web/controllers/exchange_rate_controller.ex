a84e9ecf36967dfa9ab142bcb34797ccebb4f436
adrianmarino <adrianmarino@gmail.com>
2017-02-21 00:48:01 -0300

    make code more expressive and improve error management

--- web/controllers/exchange_rate_controller.ex
+++ web/controllers/exchange_rate_controller.ex
@@ -7,8 +7,8 @@ defmodule Point.ExchangeRateController do
 
   def show(conn, %{"source" => source_code, "target" => target_code}) do
     case ExchangeRateService.by(source_code: source_code, target_code: target_code) do
-      {:error, _}  -> send_resp(conn, :not_found, "")
-      {:ok, exchange_rate} -> render(conn, "show.json", exchange_rate: exchange_rate)
+      nil -> send_resp(conn, :not_found, "")
+      exchange_rate -> render(conn, "show.json", exchange_rate: exchange_rate)
     end
   end
 
@@ -25,13 +25,14 @@ defmodule Point.ExchangeRateController do
   end
 
   def update(conn, %{"source" => source_code, "target" => target_code}) do
-    case ExchangeRateService.update(source_code: source_code, target_code: target_code,
-      value: conn.body_params["value"]) do
+    value = conn.body_params["value"]
+    case ExchangeRateService.update(source_code: source_code, target_code: target_code, value: value) do
       {:ok, exchange_rate} ->
         conn
           |> put_status(:created)
           |> put_resp_header("localtion", exchange_rate_path(conn, :show, source_code, target_code))
           |> render("show.json", exchange_rate: exchange_rate)
+      {:error, :not_found} -> send_resp(conn, :not_found, "")
       {:error, changeset} ->
         conn |> put_status(:unprocessable_entity) |> render(ChangesetView, "error.json", changeset: changeset)
     end
@@ -40,7 +41,8 @@ defmodule Point.ExchangeRateController do
   def delete(conn, %{"source" => source_code, "target" => target_code}) do
     case ExchangeRateService.delete(source_code: source_code, target_code: target_code) do
       {:ok, _ } -> send_resp(conn, :no_content, "")
-      {:error, message } -> send_message_resp(conn, :not_found, message)
+      {:error, :not_found } -> send_message_resp(conn, :not_found, "")
+      {:error, message } -> send_message_resp(conn, :internal_server_error, message)
     end
   end
 end
